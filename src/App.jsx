5a22ee3061e0ac6d1f1300fdb41fdce5430b4d5b
Anton Korniievskyi <anton.korniievskyi.cr@gmail.com>
2023-08-15 23:19:46 +0300

    Map implemented

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { ReactComponent as UkraineMap } from './assets/ua.svg';
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('#663399');
+
+  const handleClick = ({ target }) => {
+    if (target.tagName === 'path') {
+      !target.style.fill
+        ? target.style.fill = color
+        : target.style.fill = '';
+    }
+  };
+
+  return (
+    <section>
+      <div>
+        <UkraineMap onClick={handleClick} />
+      </div>
+      <input value={color} type='color' onChange={({ target }) => setColor(target.value)} />
+      <p className='text'>
+        anton.korniievskyi
+      </p>
+    </section>
+  );
+}
+
+export default App;
